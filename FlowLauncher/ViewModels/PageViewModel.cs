e0ee452849982053daaa2ced9ac33a7b16d00256
任天天 <ruattqwq@gmail.com>
2026-05-12 21:36:33 +0800

    feat(nav): support left menu item binding to specific content

--- FlowLauncher/ViewModels/PageViewModel.cs
+++ FlowLauncher/ViewModels/PageViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FlowLauncher.Platforms;
 
 namespace FlowLauncher.ViewModels;
 
 public abstract partial class PageViewModel(string id, string title = "Untitled") : ViewModelBase
 {
+    public static RootLayoutViewModel RootLayout => BaseWindow.RootLayout;
+
     public string Id { get; } = id;
 
     public string Title { get; protected set => SetProperty(ref field, value); } = title;
@@ -18,7 +23,7 @@ public abstract partial class PageViewModel(string id, string title = "Untitled"
 
     [ObservableProperty] private Control? _content = null;
 
-    protected static Geometry? GetIcon(string resourceKey)
+    protected static Geometry? Icon(string resourceKey)
     {
         if (Application.Current?.Resources is not { } res) return null;
         if (!res.TryGetValue(resourceKey, out var value)) return null;
